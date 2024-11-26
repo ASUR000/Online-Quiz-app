ce6009ea66318291085f696353d1232f49f56b4d
ASUR000 <sidyaduvanshi67@gmail.com>
2024-11-26 20:15:28 +0530

    Add files via upload

---
+++ Question.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Question {
+    private int id;
+    private String questionText;
+    private String optionA;
+    private String optionB;
+    private String optionC;
+    private String optionD;
+    private char correctOption;
+
+    public Question(int id, String questionText, String optionA, String optionB, String optionC, String optionD, char correctOption) {
+        this.id = id;
+        this.questionText = questionText;
+        this.optionA = optionA;
+        this.optionB = optionB;
+        this.optionC = optionC;
+        this.optionD = optionD;
+        this.correctOption = correctOption;
+    }
+
+    // Getters and setters
+}
---
+++ script.js
@@ -0,0 +1,3 @@
+function submitQuiz() {
+    alert("Quiz submitted!");
+}
